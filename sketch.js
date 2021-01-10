815d225ce869d8a871fd51139a98282be4932edb
Hardik9b <76246710+Hardik9b@users.noreply.github.com>
2021-01-10 13:47:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+const Engine = Matter.Engine; 
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine,world;
+var ground;
+var ball
+function setup() {
+  createCanvas(400,400);
+  //createSprite(400, 200, 50, 50);
+  engine = Engine.create();
+  world = engine.world;
+
+  var ground_options = {
+    isStatic :true
+  }
+
+  ground = Bodies.rectangle(200,380,400,30,ground_options);
+  World.add(world,ground);
+  
+  var ball_options ={
+    restitution : 1.2
+
+  }
+
+
+  
+ball = Bodies.circle(200,200,20,ball_options)
+World.add(world,ball)
+  console.log(ground);
+  console.log(ball);
+  }
+
+function draw() {
+  background(0);
+  Engine.update(engine);
+  rectMode(CENTER);
+  rect(ground.position.x,ground.position.y,400,30)  
+  ellipseMode(RADIUS)
+  ellipse(ball.position.x,ball.position.y,20,20)
+  drawSprites();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
